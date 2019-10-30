45aa121cee3c4f1c36dd913f49db805c11e8304e
Onur Kantar <onur.kantar@ceng.deu.edu.tr>
2019-10-30 13:10:42 +0300

    ignoreFolder klasörü eklendi git e yüklenmek istenilmeyenler buraya atılacak

--- videoCapture.py
+++ videoCapture.py
@@ -1,31 +1,22 @@
-'''
-Using OpenCV takes a mp4 video and produces a number of images.
-
-Requirements
-----
-You require OpenCV 3.2 to be installed.
-
-Run
-----
-Open the main.py and edit the path to the video. Then run:
-$ python main.py
-
-Which will produce a folder called data with the images. There will be 2000+ images for example.mp4.
-'''
+from PIL import Image, ImageDraw
+import face_recognition
 import cv2
 import numpy as np
+import glob
 import os
+ 
+
+
 
 # Playing video from file:
-cap = cv2.VideoCapture('testVideo.mp4')
-
+cap = cv2.VideoCapture('./ignoreFolder/testVideo.mp4')
 try:
     if not os.path.exists('data'):
         os.makedirs('data')
 except OSError:
     print ('Error: Creating directory of data')
 
-currentFrame = 0
+frameNumber = 1
 while(True):
     # Capture frame-by-frame
     
@@ -34,15 +25,32 @@ while(True):
         break
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Saves image of the current frame in jpg file
-    name = './data/frame' + str(currentFrame) + '.jpg'
-    print ('Creating...' + name)
-    cv2.imwrite(name, frame)
+    #name = './data/frame' + str(currentFrame) + '.jpg'
+    #print ('Creating...' + name)
+    #cv2.imwrite(name, frame)
 
     # To stop duplicate images
-    currentFrame += 1
-    
-    
+    #currentFrame += 1
+    img = np.zeros([720,1080,3],dtype=np.uint8)
+    pil_image = Image.fromarray(img)
+    d = ImageDraw.Draw(pil_image)
+    face_landmarks_list = face_recognition.face_landmarks(frame)
+    for face_landmarks in face_landmarks_list:
+        print("frame {}".format(frameNumber))
+        print("The top lip in this face has the following points: {}".format(face_landmarks["top_lip"]))   
+        print("The bottom lip in this face has the following points: {}".format(face_landmarks["bottom_lip"]))   
+        print("\n")
+    d.line(face_landmarks["top_lip"], width=5)
+    d.line(face_landmarks["bottom_lip"], width=5)
+    name = './data/frame' + str(frameNumber) + '.jpg'
+    pil_image.save(name)
+    frameNumber +=1
+
+cap.release()
+
+# Load the jpg file into a numpy array
+
+# Find all facial features in all the faces in the image
+#face_landmarks_list = face_recognition.face_landmarks(image)
 
 # When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
